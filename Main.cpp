5bcdbb4fad678c1f5e5f11d05a2b1dffa31dda57
JoonatanSalo <joonatan.r.salo.com>
2020-02-21 20:41:18 +0200

    added win and draw conditions and a random enemy.

--- Main.cpp
+++ Main.cpp
@@ -11,32 +11,18 @@ public:
 	}
 
 public:
-	bool OnUserCreate() override
-	{
-		return true;
-	}
-
-	/*std::vector<std::string> GameField = 
-	{" ", " ", " ",
-	 " ", " ", " ",
-	 " ", " ", " ",};
-	 */
-
-	/*int GameField[3][3] = {
-		{1, 1, 1},
-		{1, 1, 1},
-		{1, 1, 1},
-	};
-	*/
 
 	char GameField[3][3] = {
-		{' ', ' ', 'x'},
-		{'o', 'x', ' '},
-		{' ', ' ', 'o'},
+		{' ', ' ', ' '},
+		{' ', ' ', ' '},
+		{' ', ' ', ' '},
 	};
 
 	int x;
 	int y;
+
+	enum gameState { inProgress, Won, Lost, Draw };
+	gameState state;
 
 	int GetMouseXY()
 	{
@@ -51,6 +37,100 @@ public:
 		array[y][x] = replace;
 	};
 
+	bool BoardHasSpace()
+	{
+		bool hasSpace = false;
+
+		for (int m = 0; m < 3; m++)
+			for (int n = 0; n < 3; n++)
+			{
+				if (GameField[n][m] == ' ')
+				{
+					hasSpace = true;
+				}
+			}
+		if (hasSpace)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	bool CheckForWin(char z, gameState check)
+	{
+		int flag = 0;
+		
+		//rows
+		for (int y = 0; y < 3; y++)
+			for (int x = 0; x < 3; x++){
+				if (GameField[y][x] == z) {
+					flag++;
+				}
+				else {
+					flag = 0;
+				}
+
+				if (flag == 3) {
+					state = check;
+					return true;
+				}
+			}
+
+		//columns
+		flag = 0;
+		for (int x = 0; x < 3; x++)
+			for (int y = 0; y < 3; y++) {
+				if (GameField[y][x] == z) {
+					flag++;
+				}
+				else {
+					flag = 0;
+				}
+
+				if (flag == 3) {
+					state = check;
+					return true;
+				}
+			}
+
+		//cross
+		flag = 0;
+		for (int x = 0; x < 3; x++) {
+			if (GameField[x][x] == z) {
+				flag++;
+			}
+			else {
+				flag = 0;
+			}
+
+			if (flag == 3) {
+				state = check;
+				return true;
+			}
+		}
+
+		flag = 0;
+		for (int x = 0; x < 3; x++) {
+			int i = 2 - x;
+			if (GameField[i][x] == z) {
+				flag++;
+			}
+			else {
+				flag = 0;
+			}
+
+			if (flag == 3) {
+				state = check;
+				return true;
+			}
+		}
+
+		return false;
+	}
+	
 	void DrawBoard()
 	{
 		for (int x = 0; x < 3; x++)
@@ -71,8 +151,28 @@ public:
 					DrawRect(25 + x * 60, 25 + y * 60, 60, 60, olc::WHITE);
 				}
 			}
-
 	};
+
+	void OpponentsTurn()
+	{
+		while (true)
+		{
+			int i = rand() % 3;
+			int j = rand() % 3;
+
+			if (GameField[j][i] == ' ')
+			{
+				pop(GameField, i, j, 'o');
+				break;
+			}
+		}
+	};
+
+	bool OnUserCreate() override
+	{
+		state = inProgress;
+		return true;
+	}
 
 	bool OnUserUpdate(float fElapsedTime) override
 	{
@@ -85,40 +185,92 @@ public:
 
 		DrawBoard();
 
-		if (GetMouse(0).bPressed)
+
+		switch (state)
 		{
-			//x = GetMouseX() / 85;
-			//y = GetMouseY() / 85;
-
-			x, y = GetMouseXY();
-
-			if (GameField[y][x] == ' ')
+		case (inProgress):
+			if (GetMouse(0).bPressed)
 			{
-				pop(GameField, x, y, 'x');
-			}
+				x, y = GetMouseXY();
 
-			else if (GameField[y][x] == ' ')
+				if (GameField[y][x] == ' ')
+				{
+					pop(GameField, x, y, 'x');
+					if (!CheckForWin('x', Won))
+					{
+						if (BoardHasSpace())
+						{
+							OpponentsTurn();
+							//CheckForWin('o', Lost);
+						}
+						else if (!BoardHasSpace())
+						{
+							state = Draw;
+						}
+					}
+				}
+			}
+			break;
+		case Draw:
+			DrawString(1, 2, "It's a draw!", olc::BLACK, 1);
+			DrawString(1, 13, "Press 'space' for a new game.", olc::RED, 1);
+
+			if (GetKey(olc::SPACE).bPressed)
 			{
-				pop(GameField, x, y, 'x');
+				for (int x = 0; x < 3; x++)
+					for (int y = 0; y < 3; y++)
+					{
+						pop(GameField, x, y, ' ');
+					}
+				state = inProgress;
 			}
+			break;
 
+		case Won:
+			DrawString(1, 2, "You won!", olc::BLACK, 1);
+			DrawString(1, 13, "Press 'space' for a new game.", olc::RED, 1);
+
+			if (GetKey(olc::SPACE).bPressed)
+			{
+				for (int x = 0; x < 3; x++)
+					for (int y = 0; y < 3; y++)
+					{
+						pop(GameField, x, y, ' ');
+					}
+				state = inProgress;
+			}
+			break;
+
+		case Lost:
+			DrawString(1, 2, "You lost!", olc::BLACK, 1);
+			DrawString(1, 13, "Press 'space' for a new game.", olc::RED, 1);
+
+			if (GetKey(olc::SPACE).bPressed)
+			{
+				for (int x = 0; x < 3; x++)
+					for (int y = 0; y < 3; y++)
+					{
+						pop(GameField, x, y, ' ');
+					}
+				state = inProgress;
+			}
+			break;
+
+
+			//DrawString(1, 2, "x: " + std::to_string(x), olc::BLACK, 1);
+			//DrawString(1, 13, "y: " + std::to_string(y), olc::RED, 1);
+
+			return true;
 		}
-		DrawString(1, 2, "x: " + std::to_string(x), olc::RED, 1);
-		DrawString(1, 13, "y: " + std::to_string(y), olc::BLACK, 1);
-
-
-		
-			
-		return true;
 	}
 };
 
 
-int main()
-{
-	TicTacToe demo;
-	if (demo.Construct(240, 240, 4, 4)) 
-		demo.Start();
+	int main()
+	{
+		TicTacToe demo;
+		if (demo.Construct(240, 240, 4, 4))
+			demo.Start();
 
-	return 0;
-}
+		return 0;
+	}
